43ad7adbf98959f435fae778cd3ec174b264dd26
bariabdul <abdul.bari.1@outlook.com>
2018-01-12 20:10:28 -0800

    Added segues programmatically for third VC

---
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,33 @@
+//
+//  LeagueVC.swift
+//  Swoosh
+//
+//  Created by Bari Abdul on 1/12/18.
+//  Copyright © 2018 Bari Abdul. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
