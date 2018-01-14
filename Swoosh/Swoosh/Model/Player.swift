26fba81bae4a81aba5d6f7d42e870416408fb728
bariabdul <abdul.bari.1@outlook.com>
2018-01-14 12:55:50 -0800

    finished with segues passing data

---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Bari Abdul on 1/12/18.
+//  Copyright © 2018 Bari Abdul. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
